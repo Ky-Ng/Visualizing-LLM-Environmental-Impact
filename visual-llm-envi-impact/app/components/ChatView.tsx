35934dca622d2821701272da604e0b00c7e929c3
Kyle Ng <kgng@usc.edu>
2024-11-25 09:39:21 -0800

    count word length

--- visual-llm-envi-impact/app/components/ChatView.tsx
+++ visual-llm-envi-impact/app/components/ChatView.tsx
@@ -9,9 +9,27 @@ type Props = {}
 
 export default function ChatView({ }: Props) {
     const { messages, input, handleInputChange, handleSubmit } = useChat()
+
+    function getWordCount(str: string): number {
+        // https://stackoverflow.com/questions/18679576/counting-words-in-string
+        return str.trim().split(/\s+/).length
+    }
+
+    function calculateLength(messages: Message[]) {
+        /**
+         * Calculates the length of the `msg.content` for both the 
+         * Assistant and User roles and returns the sum of the total number of words
+         */
+        const totalWordCount = messages.reduce((accumulator, msg) => {
+            return accumulator + getWordCount(msg.content)
+        }, 0)
+        return totalWordCount
+    }
+
     return (
         <div>
             <p>Chat View Component</p>
+            <p>Total Chat Length in words: {calculateLength(messages)}</p>
             {messages.map((m: Message) => {
                 return (
                     <div key={m.id}>
